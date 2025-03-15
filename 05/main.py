71f155fb122e64c274773b3dad462eb5df359b8d
A-Tagir <191856809+A-Tagir@users.noreply.github.com>
2025-03-15 13:04:47 +0300

    Task2

--- 05/main.py
+++ 05/main.py
@@ -1,7 +1,7 @@
 import sentry_sdk
 
 sentry_sdk.init(
-    dsn="https://673e5ab0c1ddaa2d972f868443c260a8@o4508981080555520.ingest.de.sentry.io/4508981084815440",
+    dsn="https://60805645ed123d74623b4a12570a6dc2@o4508981080555520.ingest.de.sentry.io/4508981129052240",
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
@@ -11,4 +11,3 @@ sentry_sdk.init(
 )
 if __name__ == "__main__":
     division_zero = 1 /0
-    
